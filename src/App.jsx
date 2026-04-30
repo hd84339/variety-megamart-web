edc8e6a3dc7332de882fbf13edb318c6412c4544
harsh dubey <hd84339@gmail.com>
2026-04-30 12:21:48 +0530

    Initial setup with Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-primary">
+        Variety Megamart
+      </h1>
+    </div>
+  );
+}
+
+export default App;
